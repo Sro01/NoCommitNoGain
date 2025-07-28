4b994d32cb6b27a2a858ba6f6785619a80bae555
Sro01 <sara0099@naver.com>
2025-07-28 17:32:18 +0900

    init: CORS 오류 수정
    <manifest.json>
    permissions 추가: activeTab 권한을 추가하여 기본적인 탭 접근 권한을 부여
    host_permissions 추가: 모든 사이트에 대한 접근 권한 추가

---
+++ src/popup/Popup.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Popup: React.FC = () => (
+    <div>
+        <h1>No Commit No Gain</h1>
+        <p>No Commit No Gain</p>
+        <p>No Commit No Gain</p>
+    </div>
+);
+
+export default Popup;
